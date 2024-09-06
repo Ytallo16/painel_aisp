3ce1dd0ca6f93cbd1e2185dfd1de04bc36813cf7
ytallo gomes <ytallogomes126@gmail.com>
2024-09-06 10:38:59 -0300

    ajustando containers e sidebar

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
 import streamlit as st
-import folium
-from streamlit.components.v1 import html
 import pandas as pd
 import os
+from streamlit_folium import st_folium
+import folium
 
 st.set_page_config(page_title='Painel AIPS', page_icon='map_chart', layout='wide')
 
-# Carregar o arquivo CSV
-df = pd.read_csv('AISP.csv')
 
-# Remover linhas com NaN nas colunas críticas
-df = df.dropna(subset=['latitude', 'longitude', 'Municípios', 'BPM/CIA', 'Georeferenciamento', 'RISP', 'AISP'])
+def load_data():
+    df = pd.read_csv('AISP.csv')
+    df = df.dropna(subset=['latitude', 'longitude', 'Municípios', 'BPM/CIA', 'Georeferenciamento', 'RISP', 'AISP'])
+    df['RISP'] = df['RISP'].str.strip().str.replace(' ', '')
+    df['latitude'] = pd.to_numeric(df['latitude'], errors='coerce')
+    df['longitude'] = pd.to_numeric(df['longitude'], errors='coerce')
+    df = df.dropna(subset=['latitude', 'longitude'])
+    return df
 
-# Tratamento da coluna 'RISP' para remover espaços extras
-df['RISP'] = df['RISP'].str.strip()  # Remove espaços no início e no fim
-df['RISP'] = df['RISP'].str.replace(' ', '')  # Remove espaços internos
+df = load_data()
 
-# Converter as colunas de latitude e longitude para float
-df['latitude'] = pd.to_numeric(df['latitude'], errors='coerce')
-df['longitude'] = pd.to_numeric(df['longitude'], errors='coerce')
-
-# Remover valores NaN depois de converter
-df = df.dropna(subset=['latitude', 'longitude'])
-
-# Obter as opções únicas para cada filtro
 bpm_cia_options = df['BPM/CIA'].dropna().unique()
 georeferenciamento_options = df['Georeferenciamento'].dropna().unique()
 risp_options = df['RISP'].dropna().unique()
@@ -50,57 +44,40 @@ def get_filtered_data(municipio, bpm_cia, risp, georeferenciamento, aisp):
     
     return df_filtrado
 
-# Sidebar com filtros dentro de expanders
 with st.sidebar:
-    
     st.sidebar.header("PAINEL AISP")
     st.sidebar.image('logo_ssp.png', width=220)
     st.sidebar.subheader('Filtros')
 
 with st.sidebar.expander("Municípios"):
     municipio = st.selectbox('', options=[''] + list(municipios_options))  # Adiciona uma opção vazia para "Todos"
-    if st.button('Limpar Município'):
-        municipio = ''
+    
         
-
 with st.sidebar.expander("BPM/CIA"):
     bpm_cia = st.selectbox('', options=[''] + list(bpm_cia_options))  # Adiciona uma opção vazia para "Todos"
-    if st.button('Limpar BPM/CIA'):
-        bpm_cia = ''
+   
 
 with st.sidebar.expander("RISP"):
     risp = st.selectbox('', options=[''] + list(risp_options))  # Adiciona uma opção vazia para "Todos"
-    if st.button('Limpar RISP'):
-        risp = ''
+    
 
 with st.sidebar.expander("AISP"):
     georeferenciamento = st.selectbox('Georeferenciamento', options=[''] + list(georeferenciamento_options))  # Adiciona uma opção vazia para "Todos"
-    if st.button('Limpar Georeferenciamento'):
-        georeferenciamento = ''
-
-
     aisp = st.selectbox('AISP', options=[''] + list(aisp_options))  # Adiciona uma opção vazia para "Todos"
-    if st.button('Limpar AISP'):
-        aisp = ''
 
-# Aplicar os filtros ao DataFrame
+
 filtered_df = get_filtered_data(municipio, bpm_cia, risp, georeferenciamento, aisp)
 
-
-
-# Caminho para as imagens dos ícones personalizados
 icon_paths = {
-    'I': os.path.join(os.getcwd(), 'risp1.png'),  # Ícone para RISP I
-    'II': os.path.join(os.getcwd(), 'risp2.png'),  # Ícone para RISP II
-    'III': os.path.join(os.getcwd(), 'risp3.png'),  # Ícone para RISP III
-    'IV': os.path.join(os.getcwd(), 'risp4.png')   # Ícone para RISP IV
+    'I': os.path.join(os.getcwd(), 'risp1.png'),
+    'II': os.path.join(os.getcwd(), 'risp2.png'),
+    'III': os.path.join(os.getcwd(), 'risp3.png'),
+    'IV': os.path.join(os.getcwd(), 'risp4.png')
 }
 
-# Criar o mapa
-if not filtered_df.empty:
-    mapa = folium.Map(location=[filtered_df['latitude'].mean(), filtered_df['longitude'].mean()], zoom_start=7)
+mapa = folium.Map(location=[filtered_df['latitude'].mean(), filtered_df['longitude'].mean()], zoom_start=7)
 
-    # Adicionar os pontos ao mapa com Tooltip e Popup, usando ícones personalizados
+if not filtered_df.empty:
     for _, row in filtered_df.iterrows():
         popup_html = f"""
         <div style="width: 200px; padding: 5px;">
@@ -110,22 +87,48 @@ if not filtered_df.empty:
             <b>Status:</b> {row['STATUS']}
         </div>
         """
-        # Escolher o ícone com base no valor de 'RISP'
-        icon_path = icon_paths.get(row['RISP'], os.path.join(os.getcwd(), 'default_icon.png'))  # Ícone padrão caso 'RISP' não esteja no dicionário
-        icon = folium.CustomIcon(icon_image=icon_path, icon_size=(30, 30))  # Define o tamanho do ícone
+        icon_path = icon_paths.get(row['RISP'], os.path.join(os.getcwd(), 'default_icon.png'))
+        icon = folium.CustomIcon(icon_image=icon_path, icon_size=(15, 15))
 
         folium.Marker(
             location=[row['latitude'], row['longitude']],
-            popup=folium.Popup(popup_html, max_width=300),  # Define a largura máxima do popup
+            popup=popup_html,
             tooltip=row['BPM/CIA'],
-            icon=icon  # Usa o ícone personalizado
+            icon=icon
         ).add_to(mapa)
 
-    # Converter o mapa para HTML e exibi-lo no Streamlit
-    st.title('Mapa')
-    map_html = mapa._repr_html_()
-    html(map_html, height=700, width=1100)
-    # Verificar as primeiras linhas do DataFrame filtrado
-    st.write(filtered_df)
-else:
-    st.write("Nenhum dado encontrado com os filtros aplicados.")
+
+with st.container(border=True):
+
+    st.title('Rótulos')
+    # Exibir as métricas
+    col1, col2 = st.columns(2)
+
+  
+    with col1:
+        risp_metric = st.metric("RISP", "-")
+        municipios_metric = st.metric("Municípios", "-")
+    with col2:
+        bpm_cia_metric = st.metric("BPM/CIA", "-")
+        aisp_metric = st.metric("AISP", "-")
+
+with st.container(border=True):
+
+    st.title('Mapa das RISP')
+    # Mostrar o mapa e capturar a interação
+    map_data = st_folium(mapa, width=1400, height=600)
+
+    # Atualizar métricas com base no ponto clicado
+    if map_data.get("last_object_clicked"):
+        clicked_lat = map_data["last_object_clicked"].get("lat")
+        clicked_lon = map_data["last_object_clicked"].get("lng")
+
+        clicked_point = filtered_df[(filtered_df['latitude'] == clicked_lat) & (filtered_df['longitude'] == clicked_lon)]
+        if not clicked_point.empty:
+            clicked_data = clicked_point.iloc[0]
+            risp_metric.metric("RISP", clicked_data['RISP'])
+            aisp_metric.metric("AISP", clicked_data['AISP'])
+            municipios_metric.metric("Municípios", clicked_data['Municípios'])
+            bpm_cia_metric.metric("BPM/CIA", clicked_data['BPM/CIA'])
+            
+st.write(filtered_df)
